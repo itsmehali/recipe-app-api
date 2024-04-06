99f74324dbdbcc467efb6b92a393a3919a7a1667
itsmehali <halaszbalazs9825@gmail.com>
2024-04-06 23:52:46 +0200

    Added Recipe tests

---
+++ app/recipe/serializers.py
@@ -0,0 +1,12 @@
+"""
+Serializers for recipe APIs
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
